545e52fdcab695631ee13ec605bc57bdb3f142a6
Maxinudel <166538999+Maxinudel@users.noreply.github.com>
2025-03-28 08:55:55 -0300

    Botonfunciona

---
+++ Assets/Scripts/ScriptBoton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptBoton : MonoBehaviour
+{
+    public GameObject botonGO;
+
+    private void OnMouseDown()
+    {
+        botonGO.SetActive(false);
+    }
+
+    private void OnMouseUp()
+    {
+        botonGO.SetActive(true);
+    }
+}
